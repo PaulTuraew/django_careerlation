36eeb5d600c8033479d0a618d926fa3bc1380280
paulturaew <paul.turaew@gmail.com>
2018-03-01 21:31:35 -0500

    maybe not

---
+++ careerlation/careerlation/secret_settings.py
@@ -0,0 +1,8 @@
+DATABASES = {
+   'default': {
+       'ENGINE': 'django.db.backends.mysql',
+       'NAME': 'careerlation_db',
+       'USER': 'root',
+       'PASSWORD': 'UltraInstinct18?',
+   }
+}
